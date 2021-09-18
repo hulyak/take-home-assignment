2801966fb378692df753d731de4119c322247011
hulyak <hulyakarakayaa06@gmail.com>
2021-09-17 17:58:36 -0700

    fix build error

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
       .split(" ")
       .map(function (word, index) {
         // If it is the first word make sure to lowercase all the chars.
-        if (index == 0) {
+        if (index === 0) {
           return word.toLowerCase();
         }
         // If it is not the first word only upper case the first char and lowercase the rest.
